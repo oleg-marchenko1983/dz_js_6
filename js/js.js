f6ba5ca2889b5ae7533e4f415b87e3be73d61baf
Oleg Marchenko <olegatorius1@gmail.com>
2017-12-18 09:44:02 +0200

    rework_delete comments

--- js/js.js
+++ js/js.js
@@ -1,24 +1,23 @@
 
-// /*task1*/
-// var divTask1 = document.querySelector('#divTask1');
-//
-// divTask1.onmousemove = function (event){
-//   var offSetX = document.querySelector('#offSetX');
-//   var offSetY = document.querySelector('#offSetY');
-//
-//   offSetX.innerHTML = `offSetX : ${event.offsetX}`;
-//   offSetY.innerHTML = `offSetY : ${event.offsetY}`;
-// };
-//
-// /*task2*/
-//
-// var img = document.querySelector('#imgTask2');
-// var divTask2 = document.querySelector('.divTask2');
-//
-// function openFolder (){
-// img.src = 'img/open.png';
-// }
-// divTask2.addEventListener('dblclick', openFolder);
+/*task1*/
+var divTask1 = document.querySelector('#divTask1');
+divTask1.onmousemove = function (event){
+   var offSetX = document.querySelector('#offSetX');
+   var offSetY = document.querySelector('#offSetY');
+
+   offSetX.innerHTML = `offSetX : ${event.offsetX}`;
+   offSetY.innerHTML = `offSetY : ${event.offsetY}`;
+ };
+
+ /*task2*/
+
+ var img = document.querySelector('#imgTask2');
+ var divTask2 = document.querySelector('.divTask2');
+
+ function openFolder (){
+ img.src = 'img/open.png';
+ }
+ divTask2.addEventListener('dblclick', openFolder);
 
 /*task3*/
 
@@ -51,57 +50,56 @@ function createBlocks() {
 
 /*task4*/
 
-// var divTask4iv = document.querySelector('#divTask4');
-// var imgTask4 = document.getElementsByClassName('imgTask4');
-// for (let i = 0; i < imgTask4.length; i++) {
-//   imgTask4[i].onclick = function changeBgd() {
-//     divTask4iv.style.backgroundImage = 'url(' + this.src + ')';
-//   };
-// }
-//
-// /*task5*/
-//
-// function animateBox() {
-//     var pos = 0;
-//     var box = document.getElementById('divTask5');
-//     var t = setInterval(move,30);
-//
-//     function move() {
-//         if(pos >= 50) {
-//             clearInterval(t);
-//         }
-//         else {
-//             pos += 1;
-//             divTask5.style.top = pos+'px';
-//         }
-//     }
-// }
-// 	divTask5.addEventListener("click",animateBox)
-//
-// /*task6*/
-//
-// var contTAsk6 = document.querySelector('#conteinerTask6');
-// var divTask6 = document.querySelector('#divTask6');
-//   var count = 0;
-//
-//   function runTriger() {
-//
-//     var moveRight = divTask6.style.marginRight;
-//
-//     if (moveRight === '') {
-//       divTask6.style.marginRight = '100px';
-//       divTask6.style.marginLeft = "auto";
-//       count++;
-//  	}
-//      else if (moveRight === '100px') {
-//       divTask6.style.marginRight = '0';
-//       count++;
-//
-//     } else {
-//       divTask6.style.marginLeft = "0";
-//       count = 3;
-//     }
-//
-//     console.log(count);
-//   }
-//   divTask6.addEventListener('click', runTriger);
+ var divTask4iv = document.querySelector('#divTask4');
+ var imgTask4 = document.getElementsByClassName('imgTask4');
+ for (let i = 0; i < imgTask4.length; i++) {
+   imgTask4[i].onclick = function changeBgd() {
+     divTask4iv.style.backgroundImage = 'url(' + this.src + ')';
+   };
+ }
+
+ /*task5*/
+ function animateBox() {
+     var pos = 0;
+     var box = document.getElementById('divTask5');
+     var t = setInterval(move,30);
+
+     function move() {
+         if(pos >= 50) {
+             clearInterval(t);
+         }
+         else {
+             pos += 1;
+             divTask5.style.top = pos+'px';
+         }
+     }
+ }
+	divTask5.addEventListener("click",animateBox)
+
+ /*task6*/
+
+ var contTAsk6 = document.querySelector('#conteinerTask6');
+ var divTask6 = document.querySelector('#divTask6');
+ var count = 0;
+
+   function runTriger() {
+
+     var moveRight = divTask6.style.marginRight;
+
+     if (moveRight === '') {
+       divTask6.style.marginRight = '100px';
+       divTask6.style.marginLeft = "auto";
+       count++;
+  	}
+      else if (moveRight === '100px') {
+      divTask6.style.marginRight = '0';
+      count++;
+
+     } else {
+     divTask6.style.marginLeft = "0";
+     count = 3;
+    }
+
+   console.log(count);
+  }
+   divTask6.addEventListener('click', runTriger);
